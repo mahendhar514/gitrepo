2505d43c69331901dcabe7666026a3ff1f6ee0db
Sampsa Riikonen <sampsa.riikonen@iki.fi>
2019-08-28 08:48:15 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Bootstrap scripts
+
+These scripts can be run directly from internet.  They are used to execute ```apt-get``` etc. commands.
+
+Run them before installing a certain package.  More information available on each package's site.
---
+++ gateway/init.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Packages
+sudo apt-get install ffmpeg python3-pip ipython3 libatlas-base-dev arp-scan libxml++2.6-dev libxslt1-dev autossh python3-numpy
+    
+# In order to scan more efficiently cameras, let's allow normal user to do arp scanning:    
+sudo chmod u+s /usr/sbin/arp-scan
+    
+# For systemd daemonization to work for non-root users, do:
+sudo adduser $USER systemd-journal
+sudo loginctl enable-linger $USER
+
+# This has given problems many times: should be in the default path, but many times, is not
+# Enable it right now
+export PATH=$PATH:$HOME/.local/bin
+
+# If you're running this manually, just see that the above line is in your .bashrc 
+# ..and that your run it *now*
+
+# The following is automagic fixing of the problem (don't do this manually):
+
+# Remove anything we might have added earlier to .bashrc
+grep -v "<DURANC>" $HOME/.bashrc > $HOME/.bashrc
+# Add the path to .bashrc
+echo "export PATH=$PATH:$HOME/.local/bin # <DURANC>" >> $HOME/.bashrc
