0b96b96b0e4efe9b5bed38c9c61741540f39916f
SatyaNekkanti <47356976+SatyaNekkanti@users.noreply.github.com>
2020-01-14 16:55:04 +0530

    updating the init.bash

--- dlib/init.bash
+++ dlib/init.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Packages
-sudo apt-get -y install python3-pip python3-numpy python3-opencv libopenblas-dev liblapack-dev libopencv-dev
+sudo apt-get -y install python3-pip python3-numpy python3-opencv libopenblas-dev liblapack-dev libopencv-dev cmake
