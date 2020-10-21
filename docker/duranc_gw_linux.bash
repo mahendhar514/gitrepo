792e6c7509e2f0fecc9090a2a4fbffbe1364a099
vamsiduranc <32363189+vamsiduranc@users.noreply.github.com>
2020-10-21 13:50:55 +0530

    Bash to setup Duranc Gateway Linux Docker
    Bash script to setup Duranc Gateway Linux Docker with Auto Updater

---
+++ docker/duranc_gw_linux.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+###---Bash file to setup Dockerized Duranc Gateway with Auto Updater in Linux---###
+sudo ls
+
+# Install docker compose software
+sudo apt-get install -y docker-compose
+
+# Create a link for docker config
+ln -s $HOME/.docker/config.json $HOME/.docker/auth.json
+
+# Create docker volumes for persistent storage
+docker volume create gw-files
+docker volume create motion-files
+
+# Download docker compose file
+wget -O $HOME/docker-compose-gateway-linux.yml https://github.com/DurancOy/duranc_bootstrap/blob/master/docker/docker-compose-gateway-linux.yml
+
+# Build docker containers
+docker-compose -f $HOME/docker-compose-gateway-linux.yml up -d
+
+# Show running container
+clear
+docker ps -a
