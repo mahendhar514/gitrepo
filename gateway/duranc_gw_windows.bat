89af26265e10df7cd92d178c31b5583a30e8eef1
VamsiDuranc <gitalerts@duranc.com>
2020-11-07 00:03:08 +0530

    docker-compose and batch script for windows docker

---
+++ gateway/duranc_gw_windows.bat
@@ -0,0 +1,16 @@
+REM Create a link for docker config
+mklink /H C:/Users/%USERNAME%/.docker/machine/default/auth.json C:/Users/%USERNAME%/.docker/machine/default/config.json
+
+REM Create docker volumes for persistent storage
+docker volume create edge-ai-gw-files
+docker volume create edge-ai-motion-files
+
+REM Download docker compose file
+curl -o C:/Users/%USERNAME%/docker-compose-gateway-windows.yml https://raw.githubusercontent.com/DurancOy/duranc_bootstrap/master/gateway/docker-compose-gateway-windows.yml
+
+REM Build docker containers
+docker-compose -f C:/Users/%USERNAME%/docker-compose-gateway-windows.yml up -d
+
+REM Show running container
+cls
+docker ps -a
