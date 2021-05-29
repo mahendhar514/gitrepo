9584bfbdd52416668a5d0b82e83b5382413ce1c9
VamsiDuranc <gitalerts@duranc.com>
2021-05-29 22:21:27 +0530

    updated scripts

--- gateway/docker-gw-windows.bat
+++ gateway/docker-gw-windows.bat
@@ -3,8 +3,7 @@ REM Create a link for docker config
 REM mklink /H C:/Users/%USERNAME%/.docker/auth.json C:/Users/%USERNAME%/.docker/config.json
 
 echo c6356d49-4f26-43f0-890c-75aceb6fc3ca > %USERPROFILE%/.docker/cred.txt
-cd %USERPROFILE%/.docker/
-more cred.txt | docker login --username durancai --password-stdin
+more "%USERPROFILE%/.docker/cred.txt" | docker login --username durancai --password-stdin
 
 REM Create docker volumes for persistent storage
 docker volume create --name=stg-gw-files
