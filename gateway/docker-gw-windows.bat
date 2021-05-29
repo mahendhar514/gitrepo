9844e56017b900eaa5fb7ca76f557e8a15a5fa8e
VamsiDuranc <gitalerts@duranc.com>
2021-05-29 21:46:35 +0530

    updated scripts

--- gateway/docker-gw-windows.bat
+++ gateway/docker-gw-windows.bat
@@ -3,11 +3,12 @@ REM Create a link for docker config
 REM mklink /H C:/Users/%USERNAME%/.docker/auth.json C:/Users/%USERNAME%/.docker/config.json
 
 echo c6356d49-4f26-43f0-890c-75aceb6fc3ca > %USERPROFILE%/.docker/cred.txt
-more %USERPROFILE%/.docker/cred.txt | docker login --username durancai --password-stdin
+cd %USERPROFILE%/.docker/
+more cred.txt | docker login --username durancai --password-stdin
 
 REM Create docker volumes for persistent storage
-docker volume create edge-ai-gw-files
-docker volume create edge-ai-motion-files
+docker volume create --name=stg-gw-files
+docker volume create --name=stg-motion-files
 
 REM Download docker compose file
 curl -o C:/Users/%USERNAME%/docker-compose-gateway-windows.yml https://raw.githubusercontent.com/DurancOy/duranc_bootstrap/master/gateway/docker-compose-gateway-windows.yml
