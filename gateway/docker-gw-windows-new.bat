03a6d0283ecabe7f224e7e311e533930e2566de8
VamsiDuranc <gitalerts@duranc.com>
2022-08-19 12:44:17 +0530

    updated docker scripts for windows docker

--- gateway/docker-gw-windows-new.bat
+++ gateway/docker-gw-windows-new.bat
@@ -1,20 +1,23 @@
-@echo OFF
-REM Create a link for docker config
-REM mklink /H C:/Users/%USERNAME%/.docker/auth.json C:/Users/%USERNAME%/.docker/config.json
+@echo off
+::Create a link for docker config
+::mklink /H C:/Users/%USERNAME%/.docker/auth.json C:/Users/%USERNAME%/.docker/config.json
 
 echo | set /p="2e8afdc0-884c-4446-a8ec-53642e310c09" | docker login --username durancinc --password-stdin
 
-REM Create docker volumes for persistent storage
+::Create docker volumes for persistent storage
 docker volume create --name=stg-gw-files
 docker volume create --name=stg-motion-files
 
-REM Download docker compose file
+::Download docker compose file
 curl -o C:/Users/%USERNAME%/docker-compose-gateway-windows.yml https://raw.githubusercontent.com/DurancOy/duranc_bootstrap/master/gateway/docker-compose-gateway-windows.yml
 
-REM Build docker containers
+::Build docker containers
 docker-compose -f C:/Users/%USERNAME%/docker-compose-gateway-windows.yml up -d
 
-REM Show running container
+::Delete compose file
+del C:/Users/%USERNAME%/docker-compose-gateway-windows.yml
+
+::Show running container
 docker ps -a
 
-@echo ON
+@echo on
