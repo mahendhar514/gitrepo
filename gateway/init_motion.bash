8ad1b68868f193926a2682e391dc0bf599b9f3a1
sandeep1p <sandeep.p@duranctech.com>
2023-09-19 20:26:43 +0530

    Changed nvm install to git mirror.

--- gateway/init_motion.bash
+++ gateway/init_motion.bash
@@ -251,9 +251,23 @@ then
 
 	sudo apt-get install -y curl git wget
 
-	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-	export NVM_DIR="$HOME/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+	# Set up the NVM_DIR variable and clone the NVM repository from a given branch.
+	export NVM_DIR="$HOME/.nvm" && (
+	git clone -b v0.32.1 https://portal.duranc.com/git/nvm.git "$NVM_DIR"
+	) && \. "$NVM_DIR/nvm.sh"
+
+	# Define the lines we want to ensure are in ~/.bashrc for NVM initialization.
+	NVM_LINES=(
+		'export NVM_DIR="$HOME/.nvm"'
+		'[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm'
+	)
+
+	# Loop through each line and append it to ~/.bashrc if it's not already present.
+	for LINE in "${NVM_LINES[@]}"; do
+		grep -qF -- "$LINE" ~/.bashrc || echo "$LINE" >> ~/.bashrc
+	done
+
+	source $HOME/.bashrc
 
 	nvm install $NODE_VERSION
 	nvm use v$NODE_VERSION
@@ -268,7 +282,6 @@ then
 	pm2 install pm2-logrotate
 	pm2 set pm2-logrotate:retain 7
 	pm2 set pm2-logrotate:compress true
-	source $HOME/.bashrc
 	sudo ln -s "$(which npm)" /usr/bin/npm
 else
     echo 'Node and NPM Already Installed....'
